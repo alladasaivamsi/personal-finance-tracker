63b43e06ac8eb564730098234db7ff4770987e55
alladasaivamsi <alladasaivamsi@gmail.com>
2023-07-26 18:51:31 +0530

    first commit

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { firebase } from "@firebase/app";
 import "@firebase/auth";
 import Header from "../components/Header/Header";
 import Cards from "../components/Cards/Card";
